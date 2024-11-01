03b0fad28c82994be763b2933b5cb6e52e2d57c0
wuyunbin <617772977@qq.com>
2024-11-01 12:25:05 +0800

    temp

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,153 @@
-// index.js
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+import request from '@/utils/request';
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    indicatorColor: '#fff',
+    indicatorActiveColor: '#FD4D7D',
+    indicatorNavColor: '#D8D8D8',
+    indicatorActiveNavColor: '#F02230',
+    selectIndex: 2,
+    adData: [],
+    swiperData: [],
+    navData: [],
+    store_list: [],
+    zh_page: 1,
+    zh_size: 10,
+    zh_total: 0,
+    zh_totalPage: 0,
+    latitude: '', // 纬度
+    longitude: '', // 经度
+    address: '正在获取您的地址',
+    h5ListHeight: 0,
+    appListHeight: 0,
   },
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad() {
+    this.getLocationAndData();
+    //this.getBannerData();
   },
-  onChooseAvatar(e) {
-    const { avatarUrl } = e.detail
-    const { nickName } = this.data.userInfo
-    this.setData({
-      "userInfo.avatarUrl": avatarUrl,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
+  onReachBottom() {
+    if (this.data.zh_page < this.data.zh_totalPage) {
+      this.setData({ zh_page: this.data.zh_page + 1 });
+      this.getShopData();
+    } else {
+      wx.showToast({ title: '已经到底部了', icon: 'none' });
+    }
   },
-  onInputChange(e) {
-    const nickName = e.detail.value
-    const { avatarUrl } = this.data.userInfo
-    this.setData({
-      "userInfo.nickName": nickName,
-      hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-    })
+  onPullDownRefresh() {
+    this.setData({ store_list: [], zh_page: 1 });
+    this.getShopData();
+    wx.stopPullDownRefresh();
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+
+  getLocationAndData() {
+    wx.getLocation({
+      type: 'wgs84',
       success: (res) => {
         console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+        this.setData({ latitude: res.latitude, longitude: res.longitude });
+        this.getShopData();
+        this.getAddressFromCoords();
+      },
+      fail: (res)=>{
+        console.log(res)
+      },
+    });
   },
-})
+  getAddressFromCoords() {
+    wx.request({
+      url: `member/map/getAddress/${this.data.longitude}/${this.data.latitude}/`,
+      success: (res) => {
+        const { status, data } = res.data;
+        this.setData({
+          address: status.flag ? data.addressComponent.district : '',
+        });
+      },
+    });
+  },
+
+  pageTotal(rowCount, pageSize) {
+    if (!rowCount) return 0;
+    return Math.ceil(rowCount / pageSize);
+  },
+  navigateToSearchResult(item) {
+    if (item.id) {
+      wx.navigateTo({
+        url: `../searchresult/searchresult?flag=1&id=${item.id}&name=${item.name}`,
+      });
+    }
+  },
+  scanCode() {
+    const token = wx.getStorageSync('token');
+    if (!token) {
+      wx.showToast({ icon: 'none', title: '尚未登录, 请先登录' });
+      setTimeout(() => wx.reLaunch({ url: '../verificationcodelogin/verificationcodelogin' }), 777);
+      return;
+    }
+    wx.scanCode({
+      success: (res) => {
+        wx.navigateTo({ url: res.result.split('#')[1] });
+      },
+    });
+  },
+  openWXSM() {
+    wx.hideLoading();
+    jweixin.scanQRCode({
+      needResult: 1,
+      success: (res) => {
+        wx.navigateTo({ url: res.resultStr.split('#')[1] });
+      },
+    });
+  },
+  getAdData() {
+    wx.request({
+      url: `ad/ad/index`,
+      method: 'POST',
+      data: { area_list: this.data.address },
+      success: (res) => {
+        const { status, data } = res.data;
+        this.setData({ adData: status.flag ? data : [] });
+      },
+    });
+  },
+  getBannerData() {
+    wx.showLoading({ title: '加载中' });
+    request({
+      url: `/ad/banner/index`,
+      method: 'POST',
+    }).then(res=>{
+      const { status, data } = res.data;
+      this.setData({ swiperData: status.flag ? data : [] });
+    }).catch(err=>{
+      console.log(err)
+      wx.hideLoading()
+    })
+   
+  },
+  getNavData() {
+    wx.request({
+      url: `merchant/industry/index`,
+      success: (res) => {
+        const { status, data } = res.data;
+        this.setData({ navData: status.flag ? data : [] });
+      },
+      complete: this.getAdData,
+    });
+  },
+  getShopData() {
+    wx.request({
+      url: `merchant/store/getStoresByPoint/${this.data.zh_page}/${this.data.zh_size}`,
+      method: 'POST',
+      data: { latitude: this.data.latitude, longitude: this.data.longitude },
+      success: (res) => {
+        const { status, data } = res.data;
+        if (status.flag) {
+          this.setData({
+            store_list: this.data.store_list.concat(data.storeAndTagsList),
+            zh_total: data.total,
+            zh_totalPage: this.pageTotal(data.total, this.data.zh_size),
+          });
+        }
+      },
+    });
+  },
+});
--- pages/my/index.js
+++ pages/my/index.js
@@ -1,11 +1,14 @@
 // pages/my/index.js
+import request from '@/utils/request';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    user: {
+      totalSave: 0
+    },
   },
 
   /**
@@ -26,7 +29,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this._getInfoByToken()
+    this._getSetPayConfig()
   },
 
   /**
@@ -62,5 +66,224 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+  getUrlParam(name) {
+    const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    const r = decodeURIComponent(window.location.search).substr(1).match(reg);
+    if (r !== null) {
+      return decodeURIComponent(r[2]);
+    }
+    return null;
+  },
+  onClickSetPLUSVIP() {
+    if (!wx.getStorageSync('openId') && !this.getUrlParam('code')) {
+      const url = encodeURIComponent(window.location.href)
+      wx.redirectTo({
+        url: `https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6246e0503023027f&redirect_uri=${url}&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect`
+      })
+    } else if (!wx.getStorageSync('openId') && this.getUrlParam('code')) {
+      const code = this.getUrlParam('code')
+      wx.request({
+        url: `${config.api_base_url}member/member/openid/${code}`,
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: (res) => {
+          const { status, data } = res.data
+          if (status.flag === true) {
+            wx.setStorageSync('openId', data.openid)
+            this.onClickSetPLUSVIP2()
+          }
+        }
+      })
+    } else {
+      this.onClickSetPLUSVIP2()
+    }
+  },
+  onClickSetPLUSVIP2() {
+    wx.showLoading({
+      title: '支付中...'
+    })
+    wx.request({
+      url: `${config.api_base_url}orders/orders/buyVip/198`,
+      header: {
+        'AUTH': 'ROBOT ' + wx.getStorageSync('token')
+      },
+      success: (res) => {
+        const { status, data } = res.data
+        if (status.flag) {
+          const paymentData = {
+            openId: data.memberOpenId || wx.getStorageSync('openId'),
+            ordersId: data.ordersId,
+            price: 198,
+            payAmount: 198,
+            memberId: data.memberId,
+            remark: 'vip2'
+          }
+          this.initiatePayment(paymentData)
+        } else {
+          wx.hideLoading()
+          wx.showToast({
+            icon: 'none',
+            title: '购买失败'
+          })
+        }
+      }
+    })
+  },
+  initiatePayment(paymentData) {
+    wx.request({
+      url: `${config.api_base_url}orders/pay/pay`,
+      method: 'POST',
+      data: paymentData,
+      header: {
+        'Content-Type': 'application/json'
+      },
+      success: (res) => {
+        const { status, data } = res.data
+        if (status.flag) {
+          wx.hideLoading()
+          wx.requestPayment({
+            timeStamp: data.timeStamp,
+            nonceStr: data.nonceStr,
+            package: data.package,
+            signType: data.signType,
+            paySign: data.paySign,
+            success: () => {
+              wx.showToast({
+                icon: 'none',
+                title: '支付成功'
+              })
+              this._refreshOrder(paymentData.ordersId)
+            },
+            fail: () => {
+              wx.showToast({
+                icon: 'none',
+                title: '支付取消'
+              })
+            }
+          })
+        } else {
+          wx.hideLoading()
+          wx.showToast({
+            icon: 'none',
+            title: '支付失败'
+          })
+        }
+      },
+      fail: () => {
+        wx.hideLoading()
+        wx.showToast({
+          icon: 'none',
+          title: '请求失败, 网络异常'
+        })
+      }
+    })
+  },
+  _refreshOrder(orderId) {
+    wx.request({
+      url: `${config.api_base_url}orders/pay/queryOrders/${orderId}`,
+      header: {
+        'AUTH': 'ROBOT ' + wx.getStorageSync('token')
+      },
+      success: (res) => {
+        const { status } = res.data
+        if (status.flag) {
+          this.user = {}
+          this._getInfoByToken()
+        } else {
+          wx.showToast({
+            icon: 'none',
+            title: status.msg
+          })
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          icon: 'none',
+          title: '查询订单失败'
+        })
+      }
+    })
+  },
+  _getSetPayConfig() {
+    wx.request({
+      url: `${config.api_base_url}orders/pay/config`,
+      method: 'POST',
+      data: {
+        url: window.location.href
+      },
+      success: (res) => {
+        const { status, data } = res.data
+        if (status.flag) {
+          wx.config({
+            debug: false,
+            appId: data.appid,
+            timestamp: data.timestamp,
+            nonceStr: data.nonceStr,
+            signature: data.signature,
+            jsApiList: ['checkJsApi', 'chooseWXPay']
+          })
+        }
+      }
+    })
+  },
+  onClickExit() {
+    wx.showModal({
+      content: '确定要退出登录吗',
+      success: (res) => {
+        if (res.confirm) {
+          wx.showLoading({
+            title: '正在退出'
+          })
+          setTimeout(() => {
+            wx.hideLoading()
+            wx.clearStorageSync()
+            wx.redirectTo({
+              url: `${config.base_url}`
+            })
+          }, 2000)
+        }
+      }
+    })
+  },
+  _getInfoByToken() {
+    const token = wx.getStorageSync('token')
+    if (!token) {
+      wx.showToast({
+        icon: 'none',
+        title: '尚未登录, 请先登录'
+      })
+      setTimeout(() => {
+        wx.clearStorageSync()
+        wx.reLaunch({
+          url: 'pages/login/phone/code/index'
+        })
+      }, 1000)
+      return
+    }
+    wx.showLoading({
+      title: '加载中'
+    })
+    wx.request({
+      url: `/member/member/findByToken`,
+      header: {
+        'AUTH': token
+      },
+      success: (res) => {
+        const { success, data } = res.data
+        if (success) {
+          this.user = data
+        } else {
+          wx.showToast({
+            icon: 'none',
+            title: status.msg
+          })
+        }
+      },
+      complete: () => {
+        wx.hideLoading()
+      }
+    })
   }
 })
