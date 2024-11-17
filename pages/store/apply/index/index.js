9c9fa7bd798427697279d3a0f86288196d73a676
shenjinzheng <1479330638@qq.com>
2024-11-17 23:03:50 +0800

    store/apply temp

--- pages/store/apply/index/index.js
+++ pages/store/apply/index/index.js
@@ -1,121 +1,320 @@
-// pages/merchant/apply/index/index.js
-import request from '@/utils/request';
-import { isLogin, getIdByToken } from '@/utils/common';
-import { getId } from '@/utils/auth';
-
+// pages/store/apply/index.js
 Page({
   data: {
-    merchantId: undefined,
+    industry_index: 0,
+    industry_array: [],
+    isChain: '0',
+    memberId: undefined,
+    companyName: '',
     storeName: '',
+    imageUrl: '', // 存储选择的图片URL
+    coverImageUrl: '',
     contract: '',
     phone: '',
-    address: '',
-    recommendation: '',
-    businessHoursStart: '09:00',
-    businessHoursEnd: '22:00',
-    region: ['福建省', '厦门市', '思明区'],
+    idCard: '',
+    email:'',
+    license:'',
+    bankName:'',
+    bankAccount:'',
+    accountName:'',
+    accountType:'',
+    industryId: '',
+    cityPickerValueDefault: [0, 0, 0],
+    label: '',
     provinceId: '',
     cityId: '',
-    areaId: ''
+    areaId: '',
+    province: '',
+    city: '',
+    area: '',
+    address: '',
+    recommendation: '',
+    longitude: '',
+    latitude: '',
+    businessHoursStart: '09:00',
+    businessHoursEnd: '22:00'
   },
 
   onLoad() {
-    isLogin(); // 检查登录状态
-    getIdByToken(); // 获取 Token 中的 ID
-  },
-
-  onShow() {
-    isLogin(); // 检查登录状态
-    const id = getId();
-    if (!id) {
-      console.log('获取会员编号失败');
+    const authorization = wx.getStorageSync('authorization');
+    if (!authorization || Object.keys(authorization).length === 0) {
+      wx.showToast({
+        icon: 'none',
+        title: '尚未登录, 请先登录'
+      });
+      wx.reLaunch({
+        url: '/pages/verificationcodelogin/verificationcodelogin'
+      });
       return;
     }
-    this.setData({
-      merchantId: id // 将 ID 赋值给 merchantId
-    });
-  },
-
-  onInputChange(e) {
-    const field = e.currentTarget.dataset.field; // 获取字段名称
-    this.setData({
-      [field]: e.detail.value // 更新对应字段值
-    });
+    this._getNavData(); // 获取行业
   },
 
   bindStartTimeChange(e) {
     this.setData({
-      businessHoursStart: e.detail.value // 更新开始时间
+      businessHoursStart: e.detail.value
     });
   },
 
   bindEndTimeChange(e) {
     this.setData({
-      businessHoursEnd: e.detail.value // 更新结束时间
+      businessHoursEnd: e.detail.value
     });
   },
 
-  bindRegionChange(e) {
-    const [province, city, area] = e.detail.value; // 解构省市区
+  onChangeChainNo() {
     this.setData({
-      region: e.detail.value,
-      provinceId: province,
-      cityId: city,
-      areaId: area
+      isChain: '0',
+      companyName: ''
     });
   },
 
-  apply() {
-    // 校验必填字段
-    if (!this.validateFields()) return;
+  onChangeChainYes() {
+    this.setData({
+      isChain: '1'
+    });
+  },
 
-    // 构造提交数据
-    const formData = {
-      storeName: this.data.storeName,
-      contract: this.data.contract,
-      phone: this.data.phone,
-      provinceId: this.data.provinceId,
-      cityId: this.data.cityId,
-      areaId: this.data.areaId,
-      address: this.data.address,
-      recommendation: this.data.recommendation,
-      businessHoursStart: this.data.businessHoursStart,
-      businessHoursEnd: this.data.businessHoursEnd
-    };
+  onCompanyNameInput(e) {
+    this.setData({
+      companyName: e.detail.value
+    });
+  },
 
-    // 提交入驻申请
-    request({
-      url:'/apply/merchantApply',
-      data:formData,
-      method:'post'
+  onStoreNameInput(e) {
+    this.setData({
+      storeName: e.detail.value
+    });
+  },
+
+  onContractInput(e) {
+    this.setData({
+      contract: e.detail.value
+    });
+  },
+
+  onPhoneInput(e) {
+    this.setData({
+      phone: e.detail.value
+    });
+  },
+  onidCardInput(e){
+    this.setData({
+      idCard: e.detail.value
+    });
+  },
+  onEmailInput(e){
+    this.setData({
+      email: e.detail.value
+    });
+  },
+  onLicenseInput(e){
+    this.setData({
+      license: e.detail.value
+    });
+  },
+  onBankNameInput(e){
+    this.setData({
+      bankName: e.detail.value
+    });
+  },
+  onBankAccountInput(e){
+    this.setData({
+      bankAccount: e.detail.value
+    });
+  },
+  onAccountNameInput(e){
+    this.setData({
+      accountName: e.detail.value
+    });
+  },
+  onAccountTypeInput(e){
+    this.setData({
+      accountType: e.detail.value
+    });
+  },
+  onAddressInput(e) {
+    this.setData({
+      address: e.detail.value
+    });
+  },
+  // 选择店内图片
+  chooseImage: function () {
+    wx.chooseImage({
+      count: 1, // 只允许选择1张图片
+      sizeType: ['original', 'compressed'], // 可以选择原图或压缩图
+      sourceType: ['album', 'camera'], // 可以选择相册或相机
+      success: (res) => {
+        const tempFilePath = res.tempFilePaths[0]; // 获取选择的图片路径
+        this.setData({
+          imageUrl: tempFilePath, // 设置图片路径
+        });
+
+        // 可选：上传图片到服务器
+        this.uploadImage(tempFilePath);
+      },
+      fail: (err) => {
+        wx.showToast({
+          title: '选择图片失败',
+          icon: 'none',
+        });
+      }
+    });
+  },
+  chooseCoverImage: function () {
+    wx.chooseImage({
+      count: 1, // 只允许选择1张图片
+      sizeType: ['original', 'compressed'], // 可以选择原图或压缩图
+      sourceType: ['album', 'camera'], // 可以选择相册或相机
+      success: (res) => {
+        const tempFilePath = res.tempFilePaths[0]; // 获取选择的图片路径
+        this.setData({
+          coverImageUrl: tempFilePath, // 设置图片路径
+        });
+
+        // 可选：上传图片到服务器
+        this.uploadImage(tempFilePath);
+      },
+      fail: (err) => {
+        wx.showToast({
+          title: '选择图片失败',
+          icon: 'none',
+        });
+      }
+    });
+  },
+
+  onRecommendationInput(e) {
+    this.setData({
+      recommendation: e.detail.value
+    });
+  },
+
+  onClickOpenAddress() {
+    // 打开选择地址弹窗
+    this.selectComponent('#simpleAddress').open();
+  },
+
+  onConfirmAddress(e) {
+    this.setData({
+      label: e.label,
+      provinceId: e.provinceCode,
+      cityId: e.cityCode,
+      areaId: e.areaCode,
+      province: e.provinceName,
+      city: e.cityName,
+      area: e.areaName,
+      recommendation: e.recommendation,
+      longitude: e.longitude,
+      latitude: e.latitude
+    });
+  },
+
+  onClickSubmitBtn() {
+    // if (this.data.isChain !== '0' && this.data.isChain !== '1') {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请先选择是否连锁店'
+    //   });
+    //   return;
+    // }
+
+    // if (this.data.isChain === '1' && !this.data.companyName) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入公司名称'
+    //   });
+    //   return;
+    // }
+
+    // if (!this.data.storeName) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入店铺名称'
+    //   });
+    //   return;
+    // }
+
+    // if (!this.data.contract) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入联系人'
+    //   });
+    //   return;
+    // }
+
+    // if (!this.data.phone) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入联系电话'
+    //   });
+    //   return;
+    // }
+    // if (!this.data.idCard) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入身份证号码'
+    //   });
+    //   return;
+    // }
+    // if (!this.data.email) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入邮箱'
+    //   });
+    //   return;
+    // }
+    //还有很多判断要加
+    // if (!this.data.industryId) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请选择行业'
+    //   });
+    //   return;
+    // }
+
+    // if (!this.data.address) {
+    //   wx.showToast({
+    //     icon: 'none',
+    //     title: '请输入详细地址'
+    //   });
+    //   return;
+    // }
+
+    // 执行提交申请操作(向后端发起请求)
+
+    //请求完成后跳转到列表页面
+    wx.redirectTo({
+      url: '/pages/store/apply/list/index'
     })
-      .then((res) => {
-        if(res.success){
-          setTimeout(function(){
-            wx.showToast({ icon: 'success', title: '提交成功' });
-          },1000)
-          wx.navigateTo({
-            url: '/pages/merchant/apply/list/index',
-          })
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-        wx.showToast({ icon: 'none', title: error.message || '提交失败' });
-      });
   },
 
-  validateFields() {
-    const { storeName, contract, phone, address } = this.data;
-    if (!storeName) return this.showError('请填写商家名称');
-    if (!contract) return this.showError('请填写联系人');
-    if (!phone) return this.showError('请填写联系电话');
-    if (!address) return this.showError('请输入详细地址');
-    return true;
+  industry_change(e) {
+    const index = e.detail.value;
+    this.setData({
+      industry_index: index,
+      industryId: this.data.industry_array[index].id
+    });
   },
 
-  showError(msg) {
-    wx.showToast({ icon: 'none', title: msg });
-    return false;
+  _getNavData() {
+    // 模拟获取行业数据
+    const industryData = [{
+        name: '餐饮',
+        id: 1
+      },
+      {
+        name: '零售',
+        id: 2
+      },
+      {
+        name: '教育',
+        id: 3
+      }
+    ];
+
+    this.setData({
+      industry_array: industryData
+    });
   }
 });
