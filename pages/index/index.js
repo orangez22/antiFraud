139ae56d746c83a16fcfa86c58ebd072c1e36fc2
ze <723427948@qq.com>
2024-12-18 18:22:43 +0800

    搭建好了首页的框架

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,150 +1,55 @@
-import {
-    isLogin
-} from '@/utils/common'
-import {getNearbyPage} from "@/api/merchantApi";
-
 Page({
-    data: {
-        indicatorColor: '#fff',
-        indicatorActiveColor: '#FD4D7D',
-        indicatorNavColor: '#D8D8D8',
-        indicatorActiveNavColor: '#F02230',
-        selectIndex: 2,
-        adData: [],
-        swiperData: [],
-        navData: [],
-        store_list: [],
-        zh_page: 1,
-        zh_size: 10,
-        zh_total: 0,
-        zh_totalPage: 0,
-        latitude: '', // 纬度
-        longitude: '', // 经度
-        address: '正在获取您的地址',
-        h5ListHeight: 0,
-        appListHeight: 0,
-    },
-    onLoad() {
-        this.getLocationAndData();
-        //this.getBannerData();
-    },
-    onReachBottom() {
-        if (this.data.zh_page < this.data.zh_totalPage) {
-            this.setData({
-                zh_page: this.data.zh_page + 1
-            });
-            this.getShopData();
-        } else {
-            wx.showToast({
-                title: '已经到底部了',
-                icon: 'none'
-            });
-        }
-    },
-    onPullDownRefresh() {
-        this.setData({
-            store_list: [],
-            zh_page: 1
-        });
-        this.getShopData();
-        wx.stopPullDownRefresh();
-    },
+  data: {
+    swiperList: [
+      { goods_id: 1, image_src: 'banner1.jpg' },
+      { goods_id: 2, image_src: 'banner2.jpg' },
+      { goods_id: 3, image_src: 'banner3.jpg' }
+    ],
+    searchValue: ''  // 存储搜索框的值
+  },
 
-    getLocationAndData() {
-        wx.getLocation({
-            type: 'wgs84',
-            success: (res) => {
-                console.log(res)
-                this.setData({
-                    latitude: res.latitude,
-                    longitude: res.longitude
-                });
-                this.getShopData();
-                this.getAddressFromCoords();
-            },
-            fail: (res) => {
-                console.log(res)
-            },
-        });
-    },
-    getAddressFromCoords() {
+  onLoad: function() {
+    // 页面加载时可以进行一些数据初始化
+    this.getSwiperList();
+  },
 
-    },
+  // 获取轮播图数据（模拟请求数据）
+  getSwiperList() {
+    this.setData({
+      swiperList: [
+        { goods_id: 1, image_src: '/banner1.png' },
+        { goods_id: 2, image_src: '/banner2.png' },
+        { goods_id: 3, image_src: '/banner3.png' }
+      ]
+    });
+  },
 
-    pageTotal(rowCount, pageSize) {
-        if (!rowCount) return 0;
-        return Math.ceil(rowCount / pageSize);
-    },
-    navigateToSearchResult(item) {
-        if (item.id) {
-            wx.navigateTo({
-                url: `../searchresult/searchresult?flag=1&id=${item.id}&name=${item.name}`,
-            });
-        }
-    },
-    scanCode() {
-        isLogin()
-        wx.scanCode({
-            success: (res) => {
-                wx.navigateTo({
-                    url: res.result.split('#')[1]
-                });
-            },
-        });
-    },
-    openWXSM() {
-        wx.hideLoading();
-        jweixin.scanQRCode({
-            needResult: 1,
-            success: (res) => {
-              console.log('扫码')
-              console.log(res.result)
-                wx.navigateTo({
-                    url: res.result
-                });
-            },
-        });
-    },
-    getAdData() {
+  // 搜索框输入事件
+  onSearchInput(e) {
+    this.setData({
+      searchValue: e.detail.value  // 获取搜索框的输入值
+    });
+  },
 
-    },
-    getBannerData() {
-
-    },
-    getNavData() {
-
-    },
-    getShopData() {
-        getNearbyPage({
-            latitude: this.data.latitude,
-            longitude: this.data.longitude,
-            distance: 100
-        }).then(res => {
-            if (res.success) {
-                const { records, total, size, current, pages } = res.data;
-                this.setData({
-                    store_list: this.data.zh_page === 1
-                        ? records
-                        : [...this.data.store_list, ...records],
-                    zh_total: total,
-                    zh_size: size,
-                    zh_page: current,
-                    zh_totalPage: pages
-                });
-            }
-        }).catch(err => {
-            console.log(err);
-        });
-    },
-    onClickscanCode(){
-        wx.scanCode({
-            onlyFromCamera: true,
-            success (res) {
-                console.log(res)
-                wx.navigateTo({
-                  url: res.result
-              });
-            }
-        })
+  // 搜索按钮点击事件
+  onSearchClick() {
+    const searchValue = this.data.searchValue.trim();
+    if (searchValue) {
+      wx.navigateTo({
+        url: `/subpkg/search/search?query=${searchValue}`  // 跳转到搜索结果页面
+      });
+    } else {
+      wx.showToast({
+        title: '请输入搜索内容',
+        icon: 'none'
+      });
     }
+  },
+
+  // 跳转到详情页面
+  goToDetail() {
+    wx.navigateTo({
+      url: '/subpkg/detail/detail'  // 假设有一个详情页面
+    });
+  }
 });
