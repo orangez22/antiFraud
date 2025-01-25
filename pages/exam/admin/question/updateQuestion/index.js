1ff856768d9d2ca89283951985f1a815dab2e4ff
ze <723427948@qq.com>
2025-01-25 18:58:47 +0800

    实现了管理员更新考试题目接口的连接

--- pages/exam/admin/question/updateQuestion/index.js
+++ pages/exam/admin/question/updateQuestion/index.js
@@ -12,10 +12,7 @@ Page({
 
   // 页面加载时获取考试题目列表
   onLoad: function (options) {
-    const { examId, questionId } = options; // 获取页面传递过来的 examId 和 questionId
-    if (examId) {
-      this.setData({ examId: examId }); // 保存 examId 到 data 中
-    }
+    const { questionId } = options; // 获取页面传递过来的 questionId
     if (questionId) {
       this.setData({ questionId: questionId }); // 保存 questionId 到 data 中
     }
@@ -24,13 +21,11 @@ Page({
 
   // 获取特定考试的题目
   fetchExamQuestions: function () {
-    const { currentPage, pageSize, examId, questionId, questionTypeId } = this.data;
-    console.log(questionId)
+    const { currentPage, pageSize, questionId, questionTypeId } = this.data;
     // 请求后台获取指定考试的题目列表数据
     request.post('/exam/examQuestion/list', {
       current: currentPage,
       size: pageSize,
-      examId: examId,  // 传递 examId
       questionId: questionId, // 传递 questionId
       questionTypeId: questionTypeId // 传递 questionTypeId（如果有）
     })
@@ -40,6 +35,12 @@ Page({
         if (errorCode === 20000) {
           // 请求成功，更新页面数据
           const examQuestionList = data.records || [];
+
+          // 获取 examId
+          const examId = examQuestionList[0]?.examId; // 假设返回的数据里至少有一个题目
+          if (examId) {
+            this.setData({ examId }); // 动态设置 examId
+          }
 
           // 按题目类型进行分组
           const groupedQuestions = this.groupQuestionsByType(examQuestionList);
@@ -83,18 +84,102 @@ Page({
     return grouped;
   },
 
-  // 页面下拉加载更多
-  onReachBottom: function () {
-    this.setData({ currentPage: this.data.currentPage + 1 });
-    this.fetchExamQuestions();
+  // 处理修改题目文本框的输入
+  handleQuestionTextChange: function (e) {
+    const { id } = e.currentTarget.dataset;
+    const value = e.detail.value;
+    const updatedExamQuestionList = this.data.examQuestionList.map(group => {
+      group.questions.forEach(item => {
+        if (item.id === id) {
+          item.questionText = value; // 更新题目内容
+        }
+      });
+      return group;
+    });
+    this.setData({ examQuestionList: updatedExamQuestionList });
   },
 
-  // 搜索框更新参数并重新请求数据
-  handleSearchChange: function (e) {
-    const { field } = e.currentTarget.dataset;
-    const value = e.detail.value;
-    this.setData({ [field]: value }, () => {
-      this.fetchExamQuestions(); // 每次更改搜索条件后重新请求数据
+  // 处理修改选项输入
+  handleOptionChange: function (e) {
+    const { optionKey } = e.currentTarget.dataset;
+    const { value } = e.detail;
+    const updatedExamQuestionList = this.data.examQuestionList.map(group => {
+      group.questions.forEach(item => {
+        const option = item.optionsArray.find(option => option.key === optionKey);
+        if (option) {
+          option.value = value; // 更新选项内容
+        }
+      });
+      return group;
     });
-  }
+    this.setData({ examQuestionList: updatedExamQuestionList });
+  },
+
+  // 处理修改答案输入
+  handleAnswerChange: function (e) {
+    const { id } = e.currentTarget.dataset;
+    const value = e.detail.value;
+    const updatedExamQuestionList = this.data.examQuestionList.map(group => {
+      group.questions.forEach(item => {
+        if (item.id === id) {
+          item.answer = value; // 更新答案
+        }
+      });
+      return group;
+    });
+    this.setData({ examQuestionList: updatedExamQuestionList });
+  },
+
+  // 提交更新
+  handleSubmit: function () {
+    const { examQuestionList } = this.data;
+    const updatedQuestions = [];
+  
+    examQuestionList.forEach(group => {
+      group.questions.forEach(item => {
+        // 动态生成选项对象，确保对象的键是动态的
+        const optionsObject = {};
+        
+        // 动态填充 optionsObject
+        item.optionsArray.forEach(option => {
+          optionsObject[option.key] = option.value;
+        });
+  
+        // 输出转换后的对象，查看效果
+        console.log(optionsObject);  // 在控制台看到的是动态生成的对象
+  
+        // 创建符合 ExamQuestionDTO 格式的对象
+        const examQuestionDTO = {
+          questionId: item.id,
+          questionText: item.questionText,
+          answer: item.answer,
+          options: optionsObject,  // 动态生成的选项对象
+          questionTypeId: item.questionTypeId,
+          examId: this.data.examId  // 确保传递 examId
+        };
+  
+        updatedQuestions.push(examQuestionDTO); // 添加到 updatedQuestions 数组
+      });
+    });
+  // 使用 wx.request 发送 PUT 请求
+  wx.request({
+    url: 'http://localhost:28080/exam/examQuestion/update',  // 请求的URL
+    method: 'PUT',  // 设置请求方法为 PUT
+    data: updatedQuestions[0],  // 直接发送第一个题目对象
+    header: {
+      'Content-Type': 'application/json'  // 设置请求头为 JSON 格式
+    },
+    success: (response) => {
+      const { success, errorCode } = response.data;  // 获取响应数据
+      if (errorCode === 20000) {
+        wx.showToast({ title: '更新成功', icon: 'success' });
+      } else {
+        wx.showToast({ title: '更新失败', icon: 'none' });
+      }
+    },
+    fail: () => {
+      wx.showToast({ title: '请求失败', icon: 'none' });
+    }
+  });
+}
 });
